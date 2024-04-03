57a8bb9ce446305fc06dad2b024bf861407b7012
StefanRaluca <raluca.bubulina@yahoo.com>
2024-04-03 16:52:15 +0200

    add flags language

--- src/components/GlobalState.vue
+++ src/components/GlobalState.vue
@@ -18,9 +18,31 @@ export default {
                     console.log(response.data.results);
                 })
         };
+        const urlFlag = (countriesCode) => {
+            const flagCountries = {
+                en: 'https://flagcdn.com/16x12/us.png',
+                de: 'https://flagcdn.com/16x12/de.png',
+                it: 'https://flagcdn.com/16x12/it.png',
+                ja: 'https://flagcdn.com/16x12/jp.png',
+                ko: 'https://flagcdn.com/16x12/kr.png',
+                zh: 'https://flagcdn.com/16x12/cn.png',
+                ru: 'https://flagcdn.com/16x12/ru.png',
+                es: 'https://flagcdn.com/16x12/es.png',
+                fr: 'https://flagcdn.com/16x12/fr.png',
+            };
+            // verifica se nella const flagCountries c e il flag corispondente
+            if (flagCountries.hasOwnProperty(countriesCode)) {
+                return flagCountries[countriesCode];
+            } else {
+                // se non c e  fai vedere questa img  
+                return 'https://flagcdn.com/16x12/aq.png';
+            }
+        };
+
         return {
             state: movies,
-            searchMovies
+            searchMovies,
+            urlFlag,
         };
     }
 }
@@ -31,14 +53,15 @@ export default {
         <input v-model="state.searchInput" placeholder="Type the name of the movie">
         <button @click="searchMovies">Search </button>
         <h2>Search results:</h2>
-        <ul>
-            <li v-for="movie in state.searchResults">
+        <ol>
+            <li v-for="movie in state.searchResults" :key="movie.id">
                 <p>Title: {{ movie.title }}</p>
                 <p>Original title: {{ movie.original_title }}</p>
-                <p>Language: {{ movie.original_language }}</p>
+                <p>Language: <img :src="urlFlag(movie.original_language)" alt="Flag"></p>
                 <p>Vote average: {{ movie.vote_average }}</p>
             </li>
-        </ul>
+        </ol>
     </div>
 </template>
+
 <style></style>
