4fb3c52c39288b10c89aeed7bd9e4bb57505c578
StefanRaluca <raluca.bubulina@yahoo.com>
2024-04-03 13:10:19 +0200

    add milestone 1

--- src/components/GlobalState.vue
+++ src/components/GlobalState.vue
@@ -1,15 +1,44 @@
 <script>
 import axios from 'axios';
 import { state } from '../../state.js';
+import { reactive } from 'vue';
+
 
 export default {
+    setup() {
+        // facciamo una variabile per state 
+        const movies = state;
 
+        // metodo per fare la ricerca
+        const searchMovies = () => {
+            axios
+                .get(`${movies.urlSearch}?api_key=${movies.api_key}&query=${movies.searchInput}`)
+                .then(response => {
+                    movies.searchResults = response.data.results;
+                    console.log(response.data.results);
+                })
+        };
+        return {
+            state: movies,
+            searchMovies
+        };
+    }
 }
-
 </script>
 
 <template>
-
-
+    <div>
+        <input v-model="state.searchInput" placeholder="Type the name of the movie">
+        <button @click="searchMovies">Search </button>
+        <h2>Search results:</h2>
+        <ul>
+            <li v-for="movie in state.searchResults">
+                <p>Title: {{ movie.title }}</p>
+                <p>Original title: {{ movie.original_title }}</p>
+                <p>Language: {{ movie.original_language }}</p>
+                <p>Vote average: {{ movie.vote_average }}</p>
+            </li>
+        </ul>
+    </div>
 </template>
 <style></style>
--- state.js
+++ state.js
@@ -1,8 +1,8 @@
-import { reactive } from "vue"
+import { reactive } from "vue";
 
 export const state = reactive({
-    urlSearch: 'https://api.themoviedb.org/3/search/movie?api_key=469e4b80883ef4c7db4929cf6be9c097&language=it-IT',
+    urlSearch: "https://api.themoviedb.org/3/search/movie",
     searchResults: [],
     searchInput: "",
-
+    api_key: "469e4b80883ef4c7db4929cf6be9c097",
 });
